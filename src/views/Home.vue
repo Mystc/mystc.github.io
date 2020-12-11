b131b9801eb06194024f225f28f25344e6b84941
suntianci <suntianci@microwu.com>
2020-12-11 11:12:37 +0800

    fix(module)  增加雪花

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,24 +27,71 @@
     name : 'home',
     methods:{
       /**
+       * 创建图表
+       */
+      moveEl(el){
+        let maxHeight = document.body.scrollHeight - 10;
+        let maxWidth = window.screen.width;
+        let left = Math.random()*maxWidth;
+        let top = 0;
+        el.style = 'position : absolute;top:'+top+'px;left:'+left+'px;'
+        let interval = setInterval(()=>{
+          top = top + Math.random()*5;
+          if(top<maxHeight){
+            el.style = 'position : absolute;top:'+top+'px;left:'+left+'px;'
+          }else{
+            console.log(top)
+            console.log(maxHeight)
+            el.remove()
+            clearInterval(interval)
+          }
+
+        },Math.random()*500 + 100)
+      },
+      /**
        *
        <i class="iconfont font-orange">&#xe779;</i> 狗狗
        <i class="iconfont font-red">&#xe62a;</i> 花
        <i class="iconfont font-white">&#xe607;</i> 雪花
        */
-      getIcon(){
-        let num = (Math.random()*2).toFixed(0);
-        let el = document.createElement('i');
+      getIcon(el){
+        let num = parseInt((Math.random()*2).toFixed(0));
+        let body = document.getElementById('Home');
         switch (num) {
           case 0 :
-
+            el.className = 'iconfont font-orange';
+            el.innerHTML = '&#xe779;';
+            body.append(el);
+            break;
           case 1 :
-
+            el.className = 'iconfont font-red';
+            el.innerHTML = '&#xe62a;';
+            body.append(el);
+            break;
+          case 2 :
+            el.className = 'iconfont font-white';
+            el.innerHTML = '&#xe607;';
+            body.append(el);
+            break;
         }
-      }
+        this.moveEl(el);
+      },
+      getEl(){
+        let num = parseInt((Math.random()*10).toFixed(0));
+        // let num = 1;
+        for (let i=0;i<num;i++){
+          let el = document.createElement('i');
+          this.getIcon(el)
+        }
+      },
     },
-    created(){
-      this.getIcon();
+    mounted(){
+      this.$nextTick(()=>{
+        this.getEl();
+        // setInterval(()=>{
+        //   this.getEl();
+        // },3000)
+      })
     },
     components:{
       Study,
@@ -58,6 +105,7 @@
   #Home
     width : 100vw
     height : 100vh
+    overflow : hidden
     display : flex
     padding : 50px
     background-image : linear-gradient(45deg,#757F9A,#D7DDE8)
